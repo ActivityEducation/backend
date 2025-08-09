d7d7b4bd033ab72290e64ac19076be2ce2db2555
Brandon Bleau <bmbleau@gmail.com>
2025-08-09 15:42:45 -0400

    feat: update embeded frontend app

---
+++ static/chunk-5DB6HXA2.js
@@ -0,0 +1,1 @@
+import"./chunk-2NFLSA4Y.js";var e=[{path:"template-model",loadComponent:()=>import("./chunk-M7OUHDQB.js").then(t=>t.TemplateModelEditorComponent)},{path:"",redirectTo:"template-model",pathMatch:"full"}];export{e as routes};
---
+++ static/chunk-HTAIDQ6R.js
@@ -0,0 +1,1 @@
+import{A as r,z as o}from"./chunk-QOA5MJ5H.js";var t=r({source:"Login",events:{"Create Session":o(),"Login Success":o(),"Login Failure":o(),"Set Current User":o()}});export{t as a};
